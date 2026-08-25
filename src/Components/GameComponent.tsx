f4bd0357e9d0d2595f280fc7ba3b17eb427dd821
Chris Curtis <curtischristopher0@gmail.com>
2026-08-26 00:02:10 +0100

    little glow on the pause button

--- src/Components/GameComponent.tsx
+++ src/Components/GameComponent.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import TickSystem from '../Core/TickSystem';
 import Game from '../Core/Game';
 import FuelArraySwitchesComponent from './FuelArraySwitchesComponent';
