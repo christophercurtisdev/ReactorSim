80aa7f46c0348b709ed5bbba6d3c36ed841c0228
Chris Curtis <curtischristopher0@gmail.com>
2026-09-05 01:05:48 +0100

    will finish later, clock will 100% work though

--- src/Components/ControlPanels/ClockComponent.tsx
+++ src/Components/ControlPanels/ClockComponent.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
+import Game from "../../Core/Game";
+
 export default function ClockComponent() {
+    const [angle, setAngle] = useState(0);
+
+    useEffect(() => {
+        const unsubscribeFromTickUpdates = Game.getInstance().listenToTickEvents((isRunning) => {
+            setAngle(isRunning);
+        });
+
+        return () => unsubscribeFromTickUpdates();
+    });
+
     let increments = [];
     for (let index = 0; index < 60; index++) {
         increments.push(<div className="clock-increment text-xs text-neon-pink">&#x2303;</div>);
     }
+
     let content = (<div className="clock">
+        <div className="big-hand"></div>
+        <div>
             {increments}
+        </div>
     </div>)
     return (content);
 }
